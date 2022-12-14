08e1afd9a3e0eb5dd13df679013415a110c1948d
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-12-14 17:31:44 +0300

    landingpage items

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -7,9 +7,19 @@ const LandingPage = () => {
 
   const [books, setBooks] = useState([]);
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState({})
   const baseUrl = 'http://localhost:5050/convert'
 
-  // console.log('baseUrl', baseUrl);
+  const randomPhi = books.length;
+  console.log('num', randomPhi);
+
+
+
+
+  //   items.filter(item => item.id !== id)
+  //   items.push(books[Math.floor(Math.random() * books.length)])
+
+  //   console.log(items);
 
   useEffect(() => {
     getBooks();
@@ -17,12 +27,29 @@ const LandingPage = () => {
 
   const getBooks = async () => {
     axios.get(baseUrl)
-      .then(response =>
+    .then(response =>
       setBooks(response.data.results),
       // console.log('response:', response.data)
-
       );
-  }
+    }
+
+    const items = []
+
+        for (var i=0; i<3; i++) {
+        const item = books[Math.floor(Math.random() * randomPhi)];
+        console.log(item);
+        if (!items.includes(item)) {
+          items.push(item);
+        }
+        // setResults(items);
+      }
+
+
+  console.log('items', items);
+  // useEffect(() => {
+  //   randomItems();
+  // }, [])
+
 
   const handleSearch = e => {
     setSearch(e.target.value);
@@ -45,7 +72,7 @@ const LandingPage = () => {
       </form>
        <div className='searchResults'>
         {books.map(book => {
-          console.log(book);
+          // console.log(book);
           return (
             <div key={book.id}>
               <h2>{book.author}</h2>
